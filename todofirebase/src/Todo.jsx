c81d093e3cbbd63caf320d00fbeebedb8fe7fe19
nawodyaishan <nawodyain@gmail.com>
2023-01-15 21:33:09 +0530

    Add text field to todo

--- todofirebase/src/Todo.jsx
+++ todofirebase/src/Todo.jsx
@@ -14,7 +14,7 @@ const Todo = ({todo}) => {
     return (<li className={style.li}>
         <div className={style.row}>
             <input type={"checkbox"}/>
-            <p className={style.text}>{todo}</p>
+            <p className={style.text}>{todo.text}</p>
         </div>
         <button>{<FaRegTrashAlt/>}</button>
     </li>);
